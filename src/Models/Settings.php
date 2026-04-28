d79cc2d394b42b5659ef99ff0e11ecd7191b96f2
mishu <mishu@xtelecom.ro>
2026-04-28 15:24:49 +0300

    Fix settings singleton creation

--- src/Models/Settings.php
+++ src/Models/Settings.php
@@ -20,9 +20,11 @@ class Settings extends Model
 
     public static function current(): self
     {
+        $id = Config::get('enso.algolia.settingsId');
+
         return self::$instance
-            ??= self::find(Config::get('enso.algolia.settingsId'))
-            ?? self::factory()->create();
+            ??= self::find($id)
+            ?? self::factory()->create(['id' => $id]);
     }
 
     public static function enabled(): bool
